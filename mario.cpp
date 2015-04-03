475feb0981da0e60497d248c6eeda73df528e7eb
nward3 <nward3@student02.cse.nd.edu>
2015-04-02 20:56:52 -0400

    NW - updated mario class to have crouching

--- mario.cpp
+++ mario.cpp
@@ -7,7 +7,7 @@ using namespace std;
 // mario class constructor
 Mario::Mario() {
 
-	// initialize variables
+	// initialize mario variables
 	x=0;
 	y=168;
 	frame=0;
@@ -16,6 +16,8 @@ Mario::Mario() {
 	yVel=0;
 	marioWidth=16;
 	marioHeight=32;
+	isCrouched=false;
+	isJumped=false;
 
 	//camera dimensions
 	camera.x=0;
@@ -26,13 +28,15 @@ Mario::Mario() {
 
 // adjust mario's velocity based on key pressed
 void Mario::handle_input(SDL_Event event) {
+
 	// if a key was pressed
 	if( event.type == SDL_KEYDOWN ) {
         	// adjust the velocity
         	switch( event.key.keysym.sym ) {
         		case SDLK_LEFT: xVel -= getWidth() / 4; break;	// move left
         		case SDLK_RIGHT: xVel += getWidth() / 4; break;	// move right
-			case SDLK_DOWN: xVel = 0; break;		// crouch
+			case SDLK_DOWN: isCrouched = true; break;	// crouch
+			case SDLK_x: isJumped = true; break;		// jump
         	}
 	}
 	// if a key was released
@@ -41,9 +45,62 @@ void Mario::handle_input(SDL_Event event) {
         	switch( event.key.keysym.sym ) {
             		case SDLK_LEFT: xVel += getWidth() / 4; break;	// move left
            		case SDLK_RIGHT: xVel -= getWidth() / 4; break;	// move right
-	   		case SDLK_DOWN: xVel = 0; break;		// crouch
+	   		case SDLK_DOWN: isCrouched = false; break;	// crouch
         	}
 	}
+}
+
+// set the clips for mario's motion
+void Mario::set_clips() {
+	clipsRight[0].x=0;
+	clipsRight[0].y=0;
+	clipsRight[0].w=16;
+	clipsRight[0].h=32;
+
+	clipsRight[1].x=16;
+	clipsRight[1].y=0;
+	clipsRight[1].w=16;
+	clipsRight[1].h=32;
+
+	clipsRight[2].x=16*2;
+	clipsRight[2].y=0;
+	clipsRight[2].w=16;
+	clipsRight[2].h=32;
+
+	clipsRight[3].x=16*3;
+	clipsRight[3].y=0;
+	clipsRight[3].w=16;
+	clipsRight[3].h=32;
+
+	clipsLeft[0].x=16*3;
+	clipsLeft[0].y=32;
+	clipsLeft[0].w=16;
+	clipsLeft[0].h=32;
+
+	clipsLeft[1].x=16*2;
+	clipsLeft[1].y=32;
+	clipsLeft[1].w=16;
+	clipsLeft[1].h=32;
+
+	clipsLeft[2].x=16;
+	clipsLeft[2].y=32;
+	clipsLeft[2].w=16;
+	clipsLeft[2].h=32;
+
+	clipsLeft[3].x=0;
+	clipsLeft[3].y=32;
+	clipsLeft[3].w=16;
+	clipsLeft[3].h=32;
+
+	clipsStill[1].x=0;
+	clipsStill[1].y=32*2;
+	clipsStill[1].w=16;
+	clipsStill[1].h=32;
+
+	clipsJump[1].x=16;
+	clipsJump[1].y=32*2;
+	clipsJump[1].w=16;
+	clipsJump[1].h=32;
 }
 
 // update mario's velocity to move
@@ -51,14 +108,14 @@ void Mario::move() {
 	x += xVel;	// move mario left or right
 
 	// if mario went too far to the left or right
-	if( ( x < 0 ) || ( getX() + getWidth() > getLevelW() ) ) {
+	if((x < 0) || (getX() + getWidth() > getLevelW())) {
         	x -= xVel;	// move back
 	}
 
 	y += yVel;	// move mario up or down
 
 	// if mario went too far up or down
-	if( ( y < 0 ) || ( getY() + getHeight() > getLevelH() ) ) {
+	if((y < 0 ) || (getY() + getHeight() > getLevelH())) {
 		y -= yVel;	// move back
 	}
 }
@@ -73,8 +130,11 @@ void Mario::updateStatus() {
 		status=0;
 		frame++;
 	}
-	else if (event.key.keysym.sym==SDLK_DOWN) {	// display crouching mario
+	else if (isCrouched==true) {	// display crouching mario
 		status=2;
+	}
+	else if (isJumped==true) {	// display jumping mario
+		status=3;
 	}
 	else {
 		frame=0;
@@ -155,6 +215,16 @@ SDL_Rect *Mario::getRclip() {
 // get left motion clip
 SDL_Rect *Mario::getLclip() {
 	return &clipsLeft[getFrame()];
+}
+
+// get crouch clip
+SDL_Rect *Mario::getCclip() {
+	return &clipsStill[1];
+}
+
+// get jumping clip
+SDL_Rect *Mario::getJclip() {
+	return &clipsJump[1];
 }
 
 // get camera's x coordinate
--- mario.h
+++ mario.h
@@ -4,6 +4,7 @@
 #define MARIO_H
 
 #include "graphics.h"
+#include <cmath>
 using namespace std;
 
 class Mario : public Graphics {
@@ -11,6 +12,7 @@ class Mario : public Graphics {
 public:
 	Mario();			// class constructor
 	void handle_input(SDL_Event);	// take key presses and adjusts marios's velocity
+	void set_clips();		// sets the images for mario's motion
 	void move();			// move mario
 	void updateStatus();		// update mario's position on the screen
 	void set_camera();		// set the camera over mario's position
@@ -24,6 +26,8 @@ public:
 	SDL_Event* getEvent();		// return the event from the event queue
 	SDL_Rect *getRclip();		// return mario's right motion clip
 	SDL_Rect *getLclip();		// return mario's left motion clip
+	SDL_Rect *getCclip();		// return mario's crouch clip
+	SDL_Rect *getJclip();		// return mario's jumping clip
 	int getCamerax();		// return camera's x coordinate
 	int getCameray();		// return camera's y coordinate
 
@@ -34,6 +38,8 @@ protected:
 	int status;			// direction to face
 	int marioHeight;		// mario's height
 	int marioWidth;			// mario's width
+	bool isCrouched;		// true/false that mario is crouched
+	bool isJumped;			// true/false that mario is jumping
 	SDL_Rect camera;		// camera object to view specific portion of background
 };
 
